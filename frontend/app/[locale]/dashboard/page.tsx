dee380fa0981c632c5596eee7c553f81c689ff58
Daniel-Vitorelli <danielsenevitoreli@hotmail.com>
2026-07-08 12:14:10 -0300

    chore: add Dockerfile

--- frontend/app/[locale]/dashboard/page.tsx
+++ frontend/app/[locale]/dashboard/page.tsx
@@ -1,19 +1,19 @@
-import { auth } from "@/lib/auth"; // path to your Better Auth server instance
+
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 
 async function DashboardPage() {
-  const session = await auth.api.getSession({
-    headers: await headers(), // you need to pass the headers object.
-  });
+  // const session = await auth.api.getSession({
+  //   headers: await headers(), // you need to pass the headers object.
+  // });
 
-  if(!session) {
-    redirect("/")
-  } else {
-    console.log(session)
-  }
+  // if(!session) {
+  //   redirect("/")
+  // } else {
+  //   console.log(session)
+  // }
 
-  return <div>Olá {session.user.name}</div>;
+  return <div>Olá {/*session.user.name*/}</div>;
 }
 
 export default DashboardPage;
